0100bb9a522a2ef4d859cec39821c998c547f65a
Laercio Costa Rodrigues <laercio.cr.it@gmail.com>
2023-06-09 07:03:46 +0100

    Add and edit images, Images and User on API Products

--- SuperShop/Data/Entities/Products.cs
+++ SuperShop/Data/Entities/Products.cs
@@ -9,7 +9,7 @@ namespace SuperShop.Data.Entities
         public int Id { get; set; }
 
         [Required]
-        [MaxLength(49, ErrorMessage ="The field {0} can contain 50 characteres length.")]
+        [MaxLength(49, ErrorMessage = "The field {0} can contain 50 characteres length.")]
         public string Name { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
@@ -31,5 +31,18 @@ namespace SuperShop.Data.Entities
         public double Stock { get; set; }
 
         public User User { get; set; }
+
+        public string ImageFullPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(ImageUrl))
+                {
+                    return null;
+                }
+
+                return $"https://localhost:44322{ImageUrl.Substring(1)}";
+            }
+        }
     }
 }
--- SuperShop/Data/IProductRepository.cs
+++ SuperShop/Data/IProductRepository.cs
@@ -1,8 +1,10 @@
 ﻿using SuperShop.Data.Entities;
+using System.Linq;
 
 namespace SuperShop.Data
 {
     public interface IProductRepository : IGenericyRepository<Product>
     {
+        public IQueryable GetAllWithUsers();
     }
 }
